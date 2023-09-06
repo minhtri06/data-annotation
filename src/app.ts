1441571deb4b06d3f8a2399909af47b81e4420e5
minhtri06 <pmtri.tvv@gmail.com>
2023-09-06 20:35:32 +0700

    chore: setup mongo connect for each environment

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 
 app.listen(3000, () => console.log('app is listening on port 3000'))
+console.log(process.env.MONGODB_URL)
+console.log('adsf')
