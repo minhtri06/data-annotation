4a753345734699db52e54a1a5ef270e29178e35a
minhtri06 <pmtri.tvv@gmail.com>
2023-09-08 01:59:22 +0700

    refactor: add custom request handler type

--- src/ts/types/request-handler.ts
+++ src/ts/types/request-handler.ts
@@ -1,10 +1,16 @@
 import { NextFunction, Request, Response } from 'express'
 import { IUser } from '../interfaces/user'
 
-type CustomRequest = Request & { user: IUser | Pick<IUser, '_id' | 'role'> }
+type CustomRequest = Request & { user?: IUser | Pick<IUser, '_id' | 'role'> }
 
 export type RequestHandler = (
   req: CustomRequest,
   res: Response,
-  next: NextFunction,
+  next?: NextFunction,
 ) => Promise<Response | void> | Response | void
+
+type myFunc = (a: number, b?: string) => void
+
+const yolo: myFunc = () => {}
+
+yolo(1)
