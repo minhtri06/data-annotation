dfdf76a8d6dfeb4122da9f8b5d2395f8f18e2a42
minhtri06 <pmtri.tvv@gmail.com>
2023-09-09 00:25:48 +0700

    refactor: change schema type in validator type

--- src/ts/types/validator.ts
+++ src/ts/types/validator.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi'
 
 export type Validator<Body = object, Params = object, Query = object> = {
-  body?: Joi.Schema<Body>
-  params?: Joi.Schema<Params>
-  query?: Joi.Schema<Query>
+  body?: Joi.ObjectSchema<Body>
+  params?: Joi.ObjectSchema<Params>
+  query?: Joi.ObjectSchema<Query>
 }
