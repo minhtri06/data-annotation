3eaa5fb80ed62865497dfecbc42322fe568c6680
minhtri06 <pmtri.tvv@gmail.com>
2023-09-09 21:47:53 +0700

    fix: missing redis url in env config

--- src/configs/env-config.ts
+++ src/configs/env-config.ts
@@ -8,16 +8,20 @@ class EnvConfig {
   PORT!: number
   CLIENT_URL!: string
   MONGODB_URL!: string
+  REDIS_URL!: string
+  REDIS_DEFAULT_EXPIRATION!: number
   JWT_SECRET!: string
   JWT_ACCESS_EXPIRATION_MINUTES!: number
   JWT_REFRESH_EXPIRATION_DAYS!: number
 
   constructor() {
-    const envSchema = Joi.object<EnvConfig>({
+    const envSchema = Joi.object<EnvConfig, true>({
       NODE_ENV: Joi.string().valid('dev', 'test', 'prod').required(),
       PORT: Joi.number().integer().required(),
       CLIENT_URL: Joi.string().required(),
       MONGODB_URL: Joi.string().required(),
+      REDIS_URL: Joi.string().required(),
+      REDIS_DEFAULT_EXPIRATION: Joi.number().integer().min(1),
       JWT_SECRET: Joi.string().required(),
       JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().integer().min(1),
       JWT_REFRESH_EXPIRATION_DAYS: Joi.number().integer().min(1),
