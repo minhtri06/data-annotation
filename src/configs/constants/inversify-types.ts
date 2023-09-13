9e5dc2f1011a0b264488f80ed51f4703439c336b
minhtri06 <pmtri.tvv@gmail.com>
2023-09-13 23:39:59 +0700

    refactor: bind token service to container

--- src/configs/constants/inversify-types.ts
+++ src/configs/constants/inversify-types.ts
@@ -1,5 +1,6 @@
 export const TYPES = {
   USER_SERVICE: Symbol('user-service'),
   USER_MODEL: Symbol('user-model'),
+  TOKEN_SERVICE: Symbol('token-service'),
   TOKEN_MODEL: Symbol('token-model'),
 } as const
--- src/configs/inversify-config.ts
+++ src/configs/inversify-config.ts
@@ -1,16 +1,21 @@
 import { Model } from 'mongoose'
 import { Container } from 'inversify'
-import { IUserService } from '../services/interfaces'
+import { ITokenService, IUserService } from '../services/interfaces'
 import { TYPES } from './constants'
 import { IUser } from '../models/interfaces'
-import { UserService } from '../services'
+import { TokenService, UserService } from '../services'
 import { Token, User } from '../models'
 import '../controllers'
 import { IToken } from '../models/interfaces/token.interface'
 
 const container = new Container()
-container.bind<IUserService>(TYPES.USER_SERVICE).to(UserService)
+
+// bind models
 container.bind<Model<IUser>>(TYPES.USER_MODEL).toConstantValue(User)
 container.bind<Model<IToken>>(TYPES.TOKEN_MODEL).toConstantValue(Token)
+
+// bind services
+container.bind<IUserService>(TYPES.USER_SERVICE).to(UserService)
+container.bind<ITokenService>(TYPES.TOKEN_SERVICE).to(TokenService)
 
 export default container
