6331da63c4f4830a149ddbea402435ee7eb4bc0a
minhtri06 <pmtri.tvv@gmail.com>
2023-09-13 23:26:49 +0700

    refactor: move constant folder into configs folder

--- src/configs/constants/inversify-types.ts
+++ src/configs/constants/inversify-types.ts
@@ -1,4 +1,5 @@
 export const TYPES = {
   USER_SERVICE: Symbol('user-service'),
   USER_MODEL: Symbol('user-model'),
-}
+  TOKEN_MODEL: Symbol('token-model'),
+} as const
---
+++ src/configs/constants/token.ts
@@ -0,0 +1,4 @@
+export const TOKEN_TYPES = {
+  ACCESS_TOKEN: 'access-token',
+  REFRESH_TOKEN: 'refresh-token',
+} as const
