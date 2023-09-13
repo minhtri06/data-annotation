1c556139b5dc0a75ff7a60ec6395029183c1e870
minhtri06 <pmtri.tvv@gmail.com>
2023-09-14 00:02:48 +0700

    refactor: readjust custom request type

--- src/types/custom-express.ts
+++ src/types/custom-express.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from 'express'
 import { IUser } from '../models/interfaces'
+import { Model } from 'mongoose'
 
-type CustomRequest = Request<object, object, object, object> & {
-  user?: IUser | Pick<IUser, '_id' | 'role'>
-}
+export type CustomRequest<ReqSchema> = Request<object, object, object, object> & {
+  user?: Model<IUser> | Pick<IUser, '_id' | 'role'>
+} & ReqSchema
 
 export type ReqHandler<ReqSchema = object> = (
-  req: CustomRequest & ReqSchema,
+  req: CustomRequest<ReqSchema>,
   res: Response,
   next: NextFunction,
 ) => Promise<Response | void> | Response | void
