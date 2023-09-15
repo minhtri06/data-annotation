ef04d45cfe085377ab86f99bbbd3c9bd75e4ca90
minhtri06 <pmtri.tvv@gmail.com>
2023-09-15 23:21:11 +0700

    refactor: bind auth service

--- src/configs/constants/inversify-types.ts
+++ src/configs/constants/inversify-types.ts
@@ -1,6 +1,8 @@
 export const TYPES = {
-  USER_SERVICE: Symbol('user-service'),
-  USER_MODEL: Symbol('user-model'),
-  TOKEN_SERVICE: Symbol('token-service'),
   TOKEN_MODEL: Symbol('token-model'),
+  USER_MODEL: Symbol('user-model'),
+
+  AUTH_SERVICE: Symbol('auth-service'),
+  TOKEN_SERVICE: Symbol('token-service'),
+  USER_SERVICE: Symbol('user-service'),
 } as const
--- src/configs/inversify-config.ts
+++ src/configs/inversify-config.ts
@@ -1,8 +1,8 @@
 import { Container } from 'inversify'
-import { ITokenService, IUserService } from '../services/interfaces'
+import { IAuthService, ITokenService, IUserService } from '../services/interfaces'
 import { TYPES } from './constants'
 import { IUserModel } from '../models/interfaces'
-import { TokenService, UserService } from '../services'
+import { AuthService, TokenService, UserService } from '../services'
 import { Token, User } from '../models'
 import '../controllers'
 import { ITokenModel } from '../models/interfaces/token.interface'
@@ -10,11 +10,12 @@ import { ITokenModel } from '../models/interfaces/token.interface'
 const container = new Container()
 
 // bind models
-container.bind<IUserModel>(TYPES.USER_MODEL).toConstantValue(User)
 container.bind<ITokenModel>(TYPES.TOKEN_MODEL).toConstantValue(Token)
+container.bind<IUserModel>(TYPES.USER_MODEL).toConstantValue(User)
 
 // bind services
-container.bind<IUserService>(TYPES.USER_SERVICE).to(UserService)
+container.bind<IAuthService>(TYPES.AUTH_SERVICE).to(AuthService)
 container.bind<ITokenService>(TYPES.TOKEN_SERVICE).to(TokenService)
+container.bind<IUserService>(TYPES.USER_SERVICE).to(UserService)
 
 export default container
