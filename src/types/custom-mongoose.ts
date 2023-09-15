4afcb153800f480d8c98210168e65e983c5de383
minhtri06 <pmtri.tvv@gmail.com>
2023-09-15 22:41:10 +0700

    refactor: add query option types

--- src/types/custom-mongoose.ts
+++ src/types/custom-mongoose.ts
@@ -1,7 +1,20 @@
-import { Types } from 'mongoose'
+import { Query, SortOrder, Types } from 'mongoose'
 import { ITokenModel, IUserModel } from '../models/interfaces'
-
-export type DocumentId = Types.ObjectId | string
 
 export type TokenDocument = InstanceType<ITokenModel>
 export type UserDocument = InstanceType<IUserModel>
+
+export type DocumentId = Types.ObjectId | string
+
+export type SortOption<T = { [key: string]: unknown }> = { [K in keyof T]: SortOrder }
+export type SelectOption = Parameters<typeof Query.prototype.select>[0]
+export type PopulateOption = Parameters<typeof Query.prototype.populate>[0]
+export type QueryOptions<DocType = { [key: string]: unknown }> = {
+  sortBy?: SortOption<DocType>
+  page?: number
+  limit?: number
+  select?: SelectOption
+  populate?: PopulateOption
+  lean?: boolean
+  checkPaginate?: boolean
+}
