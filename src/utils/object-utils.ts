c78838433f74862acf3e457b3e1d9fd6101ae375
minhtri06 <pmtri.tvv@gmail.com>
2023-09-16 11:55:37 +0700

    feat: add get object key util function

--- src/utils/object-utils.ts
+++ src/utils/object-utils.ts
@@ -1,3 +1,7 @@
 export const isEmptyObject = (obj: object) => {
   return Object.keys(obj).length === 0 && obj.constructor === Object
 }
+
+export const getObjectKeys = <T extends { [key: string]: unknown }>(
+  obj: T,
+): (keyof T)[] => Object.keys(obj)
