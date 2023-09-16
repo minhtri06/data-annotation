1afa61810370dc69363fae4a87ddc7bd112ff7a2
minhtri06 <pmtri.tvv@gmail.com>
2023-09-16 18:15:11 +0700

    fix: missing hash password & check role in create user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -6,6 +6,9 @@ import { ModelService } from './abstracts/model.service'
 import { IUser, IUserModel } from '../models/interfaces'
 import { TYPES } from '../configs/constants'
 import { UserDocument } from '../types'
+import { pickFields } from '../utils/object-utils'
+import { ROLES } from '../configs/role-config'
+import createHttpError from 'http-errors'
 
 @injectable()
 export class UserService extends ModelService<IUser, IUserModel> implements IUserService {
@@ -17,9 +20,27 @@ export class UserService extends ModelService<IUser, IUserModel> implements IUse
     return bcrypt.compare(rawPassword, hashedPassword)
   }
 
+  async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(password, 8)
+  }
+
   async createUser(
     body: Omit<IUser, '_id' | 'createdAt' | 'updatedAt'>,
   ): Promise<UserDocument> {
+    body = pickFields(
+      body,
+      'address',
+      'birthOfDate',
+      'name',
+      'password',
+      'phoneNumber',
+      'role',
+      'username',
+    )
+    if (body.role !== ROLES.LEVEL_1_ANNOTATOR && body.role !== ROLES.LEVEL_2_ANNOTATOR) {
+      throw createHttpError.BadRequest('Invalid role')
+    }
+    body.password = await this.hashPassword(body.password)
     return await this.Model.create(body)
   }
 }
