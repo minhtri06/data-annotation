c80f25d02e78b35e4ee60e71d29afca7bdbd291e
minhtri06 <pmtri.tvv@gmail.com>
2023-10-05 08:27:55 +0700

    test: add integration tests for create user

--- tests/integrations/user.test.ts
+++ tests/integrations/user.test.ts
@@ -12,27 +12,13 @@ import setup from '@src/setup'
 import { UserDocument } from '@src/types'
 import { generateUser } from '@tests/fixtures'
 import { setupTestDb } from '@tests/utils'
+import { User } from '@src/models'
+import { getObjectKeys } from '@src/utils'
 
 const userService = container.get<IUserService>(TYPES.USER_SERVICE)
 const tokenService = container.get<ITokenService>(TYPES.TOKEN_SERVICE)
 
 setupTestDb()
-
-let user1: UserDocument, user2: UserDocument, user3: UserDocument
-let adminUser: UserDocument
-let accessToken: string
-// let adminAccessToken: string
-beforeEach(async () => {
-  ;[user1, user2, user3] = await Promise.all([
-    userService.createUser(generateUser()),
-    userService.createUser(generateUser()),
-    userService.createUser(generateUser()),
-  ])
-  adminUser = await userService.createUser(generateUser({ role: ROLES.ADMIN }))
-
-  accessToken = tokenService.generateAccessToken(user1)
-  // adminAccessToken = tokenService.generateAccessToken(adminUser)
-})
 
 let app: Application
 let request: SuperTest<Test>
@@ -43,6 +29,20 @@ beforeAll(() => {
 
 describe('Users routes', () => {
   describe('GET /api/v1/users', () => {
+    let user1: UserDocument, user2: UserDocument, user3: UserDocument
+    let adminUser: UserDocument
+    let accessToken: string
+    beforeEach(async () => {
+      ;[user1, user2, user3] = await Promise.all([
+        userService.createUser(generateUser()),
+        userService.createUser(generateUser()),
+        userService.createUser(generateUser()),
+      ])
+      adminUser = await userService.createUser(generateUser({ role: ROLES.ADMIN }))
+
+      accessToken = tokenService.generateAccessToken(user1)
+    })
+
     it('should return 200 (ok) and a list of user', async () => {
       const res = await request
         .get('/api/v1/users')
@@ -121,6 +121,104 @@ describe('Users routes', () => {
 
     it("should return 401 if don't have access token", async () => {
       await request.get('/api/v1/users').expect(StatusCodes.UNAUTHORIZED)
+    })
+  })
+
+  describe('POST /api/v1/users', () => {
+    let adminUser: UserDocument
+    let adminAccessToken: string
+    let newRawUser: ReturnType<typeof generateUser>
+    beforeEach(async () => {
+      adminUser = await userService.createUser(generateUser({ role: ROLES.ADMIN }))
+
+      adminAccessToken = tokenService.generateAccessToken(adminUser)
+
+      newRawUser = generateUser()
+    })
+
+    it('should return 201 (created) and correctly create a user if data is ok and caller is admin', async () => {
+      await request
+        .post('/api/v1/users')
+        .send(newRawUser)
+        .set('Authorization', adminAccessToken)
+        .expect(StatusCodes.CREATED)
+
+      await expect(
+        User.countDocuments({
+          username: newRawUser.username,
+        }),
+      ).resolves.toBe(1)
+    })
+
+    it('should return user data back with correct information', async () => {
+      const res = await request
+        .post('/api/v1/users')
+        .send(newRawUser)
+        .set('Authorization', adminAccessToken)
+        .expect(StatusCodes.CREATED)
+
+      expect(res.body.user).toMatchObject({
+        username: newRawUser.username,
+        name: newRawUser.name,
+      })
+    })
+
+    it('should return 403 (forbidden) if caller is not admin', async () => {
+      const user = await userService.createUser(generateUser())
+      const accessToken = tokenService.generateAccessToken(user)
+
+      await request
+        .post('/api/v1/users')
+        .send(newRawUser)
+        .set('Authorization', accessToken)
+        .expect(StatusCodes.FORBIDDEN)
+    })
+
+    it('should return 400 (bad request) if username already exists', async () => {
+      const user = await userService.createUser(generateUser())
+      newRawUser.username = user.username
+
+      await request
+        .post('/api/v1/users')
+        .send(newRawUser)
+        .set('Authorization', adminAccessToken)
+        .expect(StatusCodes.BAD_REQUEST)
+    })
+
+    it('should return 400 (bad request) if miss required fields', async () => {
+      const requiredFields = [
+        'username',
+        'name',
+        'password',
+        'address',
+        'phoneNumber',
+      ] as const
+      for (const field of requiredFields) {
+        newRawUser = generateUser()
+        delete newRawUser[field]
+        await request
+          .post('/api/v1/users')
+          .send(newRawUser)
+          .set('Authorization', adminAccessToken)
+          .expect(StatusCodes.BAD_REQUEST)
+      }
+    })
+
+    it('should return 400 (bad request) if provide invalid fields', async () => {
+      const invalidData = {
+        monthlyAnnotation: [{ month: 1, total: 123 }],
+        avatar: 'some-link',
+        otherInvalidField: 'hello world',
+      } as const
+      for (const field of getObjectKeys(invalidData)) {
+        const invalidRawUser: Record<string, unknown> = generateUser()
+        invalidRawUser[field] = invalidData[field]
+        await request
+          .post('/api/v1/users')
+          .send(invalidRawUser)
+          .set('Authorization', adminAccessToken)
+          .expect(StatusCodes.BAD_REQUEST)
+      }
     })
   })
 })
