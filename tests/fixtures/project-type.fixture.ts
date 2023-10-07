51ce4aeedc9bdbcb8c55808e1c9d88aadf2abd1e
minhtri06 <pmtri.tvv@gmail.com>
2023-10-07 10:09:59 +0700

    test: add tests for project type service

--- tests/fixtures/project-type.fixture.ts
+++ tests/fixtures/project-type.fixture.ts
@@ -1,10 +1,12 @@
+import { faker } from '@faker-js/faker'
+
 import { IProjectType } from '@src/models/interfaces'
 
 export const generateProjectType = (
   overwriteFields: Partial<IProjectType> = {},
 ): Omit<IProjectType, '_id' | 'createdAt' | 'updatedAt'> => {
   return {
-    name: 'Machine translation',
+    name: 'Machine translation ' + faker.string.alphanumeric(10),
     ...overwriteFields,
   }
 }
---
+++ tests/units/services/project-type.service.test.ts
@@ -0,0 +1,85 @@
+import { TYPES } from '@src/configs/constants'
+import container from '@src/configs/inversify.config'
+import { Project, ProjectType } from '@src/models'
+import { IProjectTypeService } from '@src/services/interfaces/project-type.interface'
+import { ProjectTypeDocument } from '@src/types'
+import { generateProject, generateProjectType } from '@tests/fixtures'
+import { setupTestDb } from '@tests/utils'
+
+const projectTypeService = container.get<IProjectTypeService>(TYPES.PROJECT_TYPE_SERVICE)
+
+setupTestDb()
+
+describe('Project type service', () => {
+  describe('createProjectType method', () => {
+    let rawProjectType: ReturnType<typeof generateProjectType>
+    beforeEach(() => {
+      rawProjectType = generateProjectType()
+    })
+
+    it('should correctly create a project type in db', async () => {
+      const projectType = await projectTypeService.createProjectType(rawProjectType)
+      expect(projectType).toMatchObject(rawProjectType)
+      expect(ProjectType.countDocuments({ _id: projectType._id }))
+    })
+
+    it('should throw error if misses required fields', async () => {
+      await expect(
+        projectTypeService.createProjectType({} as typeof rawProjectType),
+      ).rejects.toThrow()
+    })
+
+    it('should throw error if project type name already exists', async () => {
+      await projectTypeService.createProjectType(rawProjectType)
+      const rawProjectType2 = generateProjectType({ name: rawProjectType.name })
+      await expect(
+        projectTypeService.createProjectType(rawProjectType2),
+      ).rejects.toThrow()
+    })
+  })
+
+  describe('updateProjectType method', () => {
+    let projectType: ProjectTypeDocument
+    const updatePayload = {
+      name: 'new project type name',
+    }
+    beforeEach(async () => {
+      projectType = await projectTypeService.createProjectType(generateProjectType())
+    })
+
+    it('should correctly update a project type', async () => {
+      await projectTypeService.updateProjectType(projectType, updatePayload)
+      expect(projectType.name).toBe(updatePayload.name)
+      const updatedProjectType = await projectTypeService.getOneByIdOrFail(
+        projectType._id,
+      )
+      expect(updatedProjectType.name).toBe(updatePayload.name)
+    })
+
+    it('should throw error if update with name that already exists', async () => {
+      const projectType2 = await projectTypeService.createProjectType(
+        generateProjectType(),
+      )
+      await expect(
+        projectTypeService.updateProjectType(projectType, { name: projectType2.name }),
+      ).rejects.toThrow()
+    })
+  })
+
+  describe('deleteProjectType method', () => {
+    let projectType: ProjectTypeDocument
+    beforeEach(async () => {
+      projectType = await projectTypeService.createProjectType(generateProjectType())
+    })
+
+    it('should correctly delete a project type', async () => {
+      await projectTypeService.deleteProjectType(projectType)
+      await expect(ProjectType.countDocuments({ _id: projectType._id })).resolves.toBe(0)
+    })
+
+    it('should throw error if project type has at least 1 project', async () => {
+      await Project.create(generateProject({ projectType: projectType._id }))
+      await expect(projectTypeService.deleteProjectType(projectType)).rejects.toThrow()
+    })
+  })
+})
