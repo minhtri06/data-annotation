74cfc683dd300a068cba14a2ac481d7b28fa2db9
minhtri06 <pmtri.tvv@gmail.com>
2023-10-17 22:34:10 +0700

    refactor: organize controller

--- src/controllers/request-validations/index.ts
+++
@@ -1,5 +0,0 @@
-export * as authRequestValidation from './auth.request-validation'
-export * as meRequestValidation from './me.request-validation'
-export * as projectTypeRequestValidation from './project-type.request-validation'
-export * as projectRequestValidation from './project.request-validation'
-export * as userRequestValidation from './user.request-validation'
